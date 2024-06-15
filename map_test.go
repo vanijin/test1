eb9d235586ebb5932a41d5f5361074f265b30592
vanijin <vanijin@1234.top>
2024-06-15 16:36:55 +0800

    delete

--- map_test.go
+++ map_test.go
@@ -11,31 +11,6 @@ func TestIntMinBasic(t *testing.T) {
 	}
 }
 
-//
-//func TestIntMinTableDriven(t *testing.T) {
-//	var tests = []struct {
-//		a, b int
-//		want int
-//	}{
-//		{0, 1, 0},
-//		{1, 0, 0},
-//		{2, -2, -2},
-//		{0, -1, -1},
-//		{-1, 0, -1},
-//	}
-//
-//	for _, tt := range tests {
-//
-//		testname := fmt.Sprintf("%d,%d", tt.a, tt.b)
-//		t.Run(testname, func(t *testing.T) {
-//			ans := IntMin(tt.a, tt.b)
-//			if ans != tt.want {
-//				t.Errorf("got %d, want %d", ans, tt.want)
-//			}
-//		})
-//	}
-//}
-
 func BenchmarkIntMin(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		IntMix(1, 2)
